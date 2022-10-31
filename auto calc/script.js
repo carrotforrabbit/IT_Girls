1cb9017ae5bb9b00617664a17ab734559ee754cb
carrotforrabbit <parasyuta.anna@gmail.com>
2022-10-31 10:50:19 +0000

    adds error message

--- auto calc/script.js
+++ auto calc/script.js
@@ -2,13 +2,19 @@ const form = document.querySelector('.form');
 const autoType = document.querySelector('.select');
 const services = document.querySelectorAll('.inputChoice');
 const result = document.querySelector('.results');
+const error = document.querySelector('.error');
 
 form.onsubmit = (evt) => {
-	evt.preventDefault();
+	if (autoType.value == '') {
+        error.innerHTML = 'Выбирете марку машины';
+        error.style.border = '1px solid red';
+        error.style.backgroundColor = 'lightpink';
+    } else {
+    console.log(result);
+    evt.preventDefault();
 	let total;
 	const status = autoType.value;
 	total = +status;
-	console.log(total);
 
 	for (let i = 0; i < services.length; i++) {
 		if (services[i].checked) {
@@ -16,4 +22,8 @@ form.onsubmit = (evt) => {
 		}
 	}
 	result.innerHTML = total;
+    error.innerHTML = '';
+    error.style.border = '';
+    error.style.backgroundColor = '';
+}
 };
