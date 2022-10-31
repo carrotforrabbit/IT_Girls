878e579351c3d444b54976403ae5eea949ec1660
carrotforrabbit <parasyuta.anna@gmail.com>
2022-10-31 11:18:40 +0000

    adds model change

--- auto calc/script.js
+++ auto calc/script.js
@@ -3,6 +3,25 @@ const autoType = document.querySelector('.select');
 const services = document.querySelectorAll('.inputChoice');
 const result = document.querySelector('.results');
 const error = document.querySelector('.error');
+const kia = document.querySelector('.fieldset_kia');
+const ferrari = document.querySelector('.fieldset_ferrari');
+const lambo = document.querySelector('.fieldset_lambo');
+
+function changeModel() {
+   
+    switch (document.getElementById("autoType").value)
+    {
+        case "300000" : 
+            kia.style.visibility = 'visible';
+            break;
+        case "1000000" : 
+            ferrari.style.visibility = 'visible';
+            break;
+        case "2000000" : 
+            lambo.style.visibility = 'visible'; 
+            break;
+    }
+};
 
 form.onsubmit = (evt) => {
 	if (autoType.value == '') {
